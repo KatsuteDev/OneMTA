b4247bf8a055f0a334ffdb9ce82244eda837accd
Katsute <58778985+Katsute@users.noreply.github.com>
2022-01-18 21:46:51 -0500

    adjust order

--- src/main/java/dev/katsute/onemta/MTASchema.java
+++ src/main/java/dev/katsute/onemta/MTASchema.java
@@ -94,23 +94,23 @@ abstract class MTASchema {
             private final Long end = requireNonNull(timeRange::getEnd);
 
             @Override
-            public final Long getStartEpochMillis(){
-                return start;
-            }
-
-            @Override
             public final Date getStart(){
                 return start != null ? new Date(start) : null;
             }
 
             @Override
-            public final Long getEndEpochMillis(){
-                return end;
+            public final Long getStartEpochMillis(){
+                return start;
             }
 
             @Override
             public final Date getEnd(){
                 return end != null ? new Date(end) : null;
+            }
+
+            @Override
+            public final Long getEndEpochMillis(){
+                return end;
             }
 
         };
